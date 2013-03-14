8f31dd981e2bd72a2d0025892c6d67b1c18f7cca
Matt Beckett <beck24@gmail.com>
2013-03-14 10:50:08 -0600

    adds 'my_status' to group profiles

--- lib/groups.php
+++ lib/groups.php
@@ -227,6 +227,26 @@ function groups_handle_profile_page($guid) {
 			$sidebar .= elgg_view('groups/sidebar/search', array('entity' => $group));
 		}
 		$sidebar .= elgg_view('groups/sidebar/members', array('entity' => $group));
+		
+		$subscribed = false;
+		if (elgg_is_active_plugin('notifications')) {
+			global $NOTIFICATION_HANDLERS;
+			
+			foreach ($NOTIFICATION_HANDLERS as $method => $foo) {
+				$relationship = check_entity_relationship(elgg_get_logged_in_user_guid(),
+						'notify' . $method, $guid);
+				
+				if ($relationship) {
+					$subscribed = true;
+					break;
+				}
+			}
+		}
+		
+		$sidebar .= elgg_view('groups/sidebar/my_status', array(
+			'entity' => $group,
+			'subscribed' => $subscribed
+		));
 	} else {
 		$sidebar = '';
 	}
