c008a58b7fc621ce0584c7c11a4c9965b7de79d4
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2016-12-16 14:45:33 +0100

    feat(lists): support get/list subgroup options
    Adds options to the get_subgroups() and list_subgroups()

--- views/default/resources/au_subgroups/list.php
+++ views/default/resources/au_subgroups/list.php
@@ -11,16 +11,15 @@ parent_breadcrumbs($page_owner);
 elgg_push_breadcrumb($page_owner->name, $page_owner->getURL());
 elgg_push_breadcrumb(elgg_echo('au_subgroups:subgroups'));
 
-$content = list_subgroups($page_owner, 10);
-
-if (!$content) {
-  $content = elgg_echo('au_subgroups:nogroups');
-}
+$content = list_subgroups($page_owner, [
+	'limit' => 10,
+	'no_results' => elgg_echo('au_subgroups:nogroups'),
+]);
 
 $body = elgg_view_layout('content', array(
-    'title' => $title,
-    'content' => $content,
-    'filter' => false
-));
+	'title' => $title,
+	'content' => $content,
+	'filter' => false
+		));
 
 echo elgg_view_page($title, $body);
